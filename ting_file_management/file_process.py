71a8787d28abe2bc054c8d020b24e6087f66c4f5
Paula <paulasalinor@gmail.com>
2022-12-02 10:57:02 -0300

    feat: add process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in instance._data:
+        if path_file in i["nome_do_arquivo"]:
+            return None
+
+    text = txt_importer(path_file)
+    result = {
+      "nome_do_arquivo": path_file,
+      "qtd_linhas": len(text),
+      "linhas_do_arquivo": text
+    }
+
+    instance.enqueue(result)
+    print(result)
 
 
 def remove(instance):
