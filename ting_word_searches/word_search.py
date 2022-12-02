d68b2e735618cdb62aad6bda10b5bbde60fe5050
Paula <paulasalinor@gmail.com>
2022-12-02 13:13:01 -0300

    feat: search_by_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,18 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    data = instance._data
+    for item in data:
+        occurrences = list()
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+        if len(occurrences) > 0:
+            result.append({
+              "palavra": word,
+              "arquivo": item["nome_do_arquivo"],
+              "ocorrencias": occurrences
+            })
+
+    return result
