8c99a91a9ea4d2028d71f4e24e4609c47e9268e0
Paula <paulasalinor@gmail.com>
2022-12-02 10:51:01 -0300

    feat: add Queue class

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,19 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        data_length = self.__len__()
+        if index < 0 or index > data_length - 1:
+            raise IndexError
+
+        return self._data[index]
